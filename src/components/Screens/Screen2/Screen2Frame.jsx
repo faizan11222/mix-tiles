d59b81f449b7204cca2bf7fa74fd1f237e81c22b
faizan2077 <faizanch2077@gmail.com>
2022-08-18 09:58:37 -0700

    fourth commit

--- src/components/Screens/Screen2/Screen2Frame.jsx
+++ src/components/Screens/Screen2/Screen2Frame.jsx
@@ -10,17 +10,19 @@ import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import CancelIcon from "@mui/icons-material/Cancel";
-import ClearIcon from '@mui/icons-material/Clear';
+import ClearIcon from "@mui/icons-material/Clear";
 import Cropper from "react-easy-crop";
 import ImageIcon from "@mui/icons-material/Image";
 import AspectRatioIcon from "@mui/icons-material/AspectRatio";
-import HomeIcon from '@mui/icons-material/Home';
-import PaymentIcon from '@mui/icons-material/Payment';
-import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
-import PaymentsIcon from '@mui/icons-material/Payments';
-import CreditCardInput from 'react-credit-card-input';
-
+import HomeIcon from "@mui/icons-material/Home";
+import PaymentIcon from "@mui/icons-material/Payment";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import PaymentsIcon from "@mui/icons-material/Payments";
+import App from "./Payment/App";
 const Screen2Frame = () => {
+
+
+
   const filterArr = [
     {
       key: "Classic",
@@ -76,7 +78,7 @@ const Screen2Frame = () => {
     left: false,
   });
   const [upload, setupload] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState([]);
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
   const [selectStyle, setselectStyle] = useState(false);
@@ -84,9 +86,9 @@ const Screen2Frame = () => {
   const [addAddressPopup, setaddAddressPopup] = useState(false);
   const [addPaymentMethodPopup, setaddPaymentMethodPopup] = useState(false);
   const [creditCardMethod, setcreditCardMethod] = useState(false);
+  const [sections, setsections] = useState([]);
 
   const onCropComplete = React.useCallback((croppedArea, croppedAreaPixels) => {
-    console.log(croppedArea, croppedAreaPixels);
   }, []);
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -137,39 +139,67 @@ const Screen2Frame = () => {
         <h2 className="fw-bolder mt-3 mx-3">Checkout</h2>
         <div className="check_out_list_parent">
           <ul className="list_check_out">
-            <li className="mb-4 global_hover" onClick={()=> setaddAddressPopup(true)}  > <HomeIcon className="mb-1 mx-2" fontSize="medium" /> Add Address</li>
-            <li className="mb-4 global_hover" onClick={()=>setaddPaymentMethodPopup(true)}> <PaymentIcon className="mb-1 mx-2" fontSize="medium"  /> Add Payment Method</li>
-            <li className="mb-4 text-dark"> <CardGiftcardIcon className="mb-1 mx-2" fontSize="medium" /> Add Gift Note</li>
+            <li
+              className="mb-4 global_hover"
+              onClick={() => setaddAddressPopup(true)}
+            >
+              {" "}
+              <HomeIcon className="mb-1 mx-2" fontSize="medium" /> Add Address
+            </li>
+            <li
+              className="mb-4 global_hover"
+              onClick={() => setaddPaymentMethodPopup(true)}
+            >
+              {" "}
+              <PaymentIcon className="mb-1 mx-2" fontSize="medium" /> Add
+              Payment Method
+            </li>
+            <li className="mb-4 text-dark">
+              {" "}
+              <CardGiftcardIcon className="mb-1 mx-2" fontSize="medium" /> Add
+              Gift Note
+            </li>
           </ul>
         </div>
-        <hr/>
+        <hr />
         <div>
-        <div className="mb-4 row mx-1">
-          <div className="col-2 text-primary mt-1"><CardGiftcardIcon className="mb-1 mx-2 fs-1 "  /></div>
-          <div className="col-8">
-            <p className="m-0 p-0">Promo: 12 (8”X8”) Tiles For $99</p>
-            <p className="text-primary m-0 p-0">Add 10 tiles to get the deal!</p>
+          <div className="mb-4 row mx-1">
+            <div className="col-2 text-primary mt-1">
+              <CardGiftcardIcon className="mb-1 mx-2 fs-1 " />
+            </div>
+            <div className="col-8">
+              <p className="m-0 p-0">Promo: 12 (8”X8”) Tiles For $99</p>
+              <p className="text-primary m-0 p-0">
+                Add 10 tiles to get the deal!
+              </p>
+            </div>
+            <p
+              className="col-2 mt-2 text-muted "
+              style={{ textDecoration: "underline" }}
+            >
+              cancle
+            </p>
           </div>
-            <p className="col-2 mt-2 text-muted " style={{ textDecoration:"underline" }}>cancle</p>
-        </div>
-        <hr/>
-        <div className="px-4" >
-          <div className="d-flex justify-content-between">
-            <p>2 tiles,8”X8”</p>
-            <p>$34</p>
+          <hr />
+          <div className="px-4">
+            <div className="d-flex justify-content-between">
+              <p>2 tiles,8”X8”</p>
+              <p>$34</p>
+            </div>
+            <div className="d-flex justify-content-between">
+              <p>Shipping</p>
+              <p>Free</p>
+            </div>
+            <div className="d-flex justify-content-between fw-bolder">
+              <p>Total</p>
+              <p>$34</p>
+            </div>
           </div>
-          <div className="d-flex justify-content-between">
-            <p>Shipping</p>
-            <p>Free</p>
+          <div className="p-3">
+            <button className="btn btn-primary w-100  btn-lg p-2">
+              Place Order
+            </button>
           </div>
-          <div className="d-flex justify-content-between fw-bolder">
-            <p>Total</p>
-            <p>$34</p>
-          </div>
-        </div>
-        <div className="p-3">
-        <button className="btn btn-primary w-100  btn-lg p-2">Place Order</button>
-        </div>
         </div>
       </div>
     </Box>
@@ -202,18 +232,18 @@ const Screen2Frame = () => {
           <h4>MIXTILES</h4>
         </div>
         <div>
-        <TextsmsIcon className="text-dark mx-3  " fontSize="large" />{" "}
+          <TextsmsIcon className="text-dark mx-3  " fontSize="large" />{" "}
         </div>
       </div>
 
-      {selectedImage === null && (
+      {selectedImage.length === 0 && (
         <div
           className="screen_2_body global_hover"
           onClick={() => setupload(!upload)}
         >
           <div className="screen_2_body_content">
             <div>
-              <AddIcon className="text-primary" style={{ fontSize: "100px" }} />
+              <AddIcon className="text-primary " style={{ fontSize: "100px" }} />
             </div>
           </div>
           <div
@@ -228,21 +258,31 @@ const Screen2Frame = () => {
         </div>
       )}
 
-      {selectedImage !== null && (
+      {selectedImage.length !== 0 && (
+        <div>
+
+       
+       {
+              selectedImage.map((a)=>(
         <div className="screen_2_body global_hover">
+          
           <div className="screen_2_body_content">
+            
             <button
               className="btn-remove"
-              onClick={() => setSelectedImage(null)}
+              onClick={() => setSelectedImage([])}
             >
               <CancelIcon />
             </button>
 
-            {selectedImage && (
+            {selectedImage.length !== 0 && (
+              <div>
+             
               <div className="section_2_img_frame">
+                
                 <div className="section_2_img_frame2">
                   <Cropper
-                    image={URL.createObjectURL(selectedImage)}
+                    image={URL.createObjectURL(a)}
                     crop={crop}
                     zoom={zoom}
                     aspect={1 / 1}
@@ -254,8 +294,15 @@ const Screen2Frame = () => {
                 </div>
                 <br />
               </div>
+              
+             
+             </div>
             )}
+            
           </div>
+         
+
+
           <div
             style={{
               bottom: "-85px",
@@ -264,17 +311,27 @@ const Screen2Frame = () => {
             }}
           ></div>
         </div>
+         ))}
+          </div>
       )}
 
-      {selectedImage !== null && (
+      {selectedImage.length !== 0 && (
         <div>
-          <button className="add_more_imgs">
-            <AddIcon fontSize="large" />
+          <button className="add_more_imgs"   >
+          {/* <AddIcon  /> */}
+          <input
+                type="file"
+                class="custom-file-input2"
+                onChange={(event) => {
+                  setSelectedImage([...selectedImage, event.target.files[0]]);
+                }}
+              />
+            
           </button>
         </div>
       )}
 
-      {selectedImage === null && upload && (
+      {selectedImage.length === 0 && upload && (
         <div className="screen2_upload_section">
           <div class="list-group  list_style">
             <label class="list-group-item">
@@ -286,7 +343,7 @@ const Screen2Frame = () => {
                 type="file"
                 class="custom-file-input"
                 onChange={(event) => {
-                  setSelectedImage(event.target.files[0]);
+                  setSelectedImage([...selectedImage, event.target.files[0]]);
                 }}
               />
             </label>
@@ -310,7 +367,7 @@ const Screen2Frame = () => {
 
       {/* =========================================Chunk============================================= */}
       {/* Select style btn */}
-      {selectedImage !== null && upload && (
+      {selectedImage.length !== 0 && upload && (
         <div className="screen_2_editing_section d-flex justify-content-between">
           <div class="dropdown">
             <button
@@ -404,103 +461,132 @@ const Screen2Frame = () => {
         </div>
       )}
       {/* Check out bar=============================================================== */}
-      {selectedImage !== null && (
+      {selectedImage.length !== 0 && (
         <div className="checkOut_bar text-center">
           <button className="checkOut_btn mt-3">
-          {["right"].map((anchor) => (
-            <React.Fragment key={anchor}>
-              <Button className="text-light" onClick={toggleDrawer(anchor, true)}>
-                {" "}
-                 Checkout
-              </Button>
-              <SwipeableDrawer
-                anchor={anchor}
-                open={state[anchor]}
-                onClose={toggleDrawer(anchor, false)}
-                onOpen={toggleDrawer(anchor, true)}
-              >
-                {list2(anchor)}
-              </SwipeableDrawer>
-            </React.Fragment>
-          ))}
+            {["right"].map((anchor) => (
+              <React.Fragment key={anchor}>
+                <Button
+                  className="text-light"
+                  onClick={toggleDrawer(anchor, true)}
+                >
+                  {" "}
+                  Checkout
+                </Button>
+                <SwipeableDrawer
+                  anchor={anchor}
+                  open={state[anchor]}
+                  onClose={toggleDrawer(anchor, false)}
+                  onOpen={toggleDrawer(anchor, true)}
+                >
+                  {list2(anchor)}
+                </SwipeableDrawer>
+              </React.Fragment>
+            ))}
           </button>
         </div>
       )}
 
       {/* ===============================Chunk Checkout======================================== */}
 
-
       {/* ==============================Add Address Popup ====================================== */}
       <div>
-        {
-          addAddressPopup && (
-            <div className="add_address_popup">
-              <div className="d-flex justify-content-between container px-3 mt-3 fw-bolder">
-                <div className="text-muted global_hover" onClick={()=> setaddAddressPopup(false)}><ClearIcon/></div>
-                <div>Add Address</div>
-                <div className="text-primary global_hover" >Done</div>
+        {addAddressPopup && (
+          <div className="add_address_popup">
+            <div className="d-flex justify-content-between container px-3 mt-3 fw-bolder">
+              <div
+                className="text-muted global_hover"
+                onClick={() => setaddAddressPopup(false)}
+              >
+                <ClearIcon />
               </div>
-              <hr/>
-              <div className="mx-5  address_form px-4">
-                <span>EMAIL: </span><input className=""/>
-                <hr/>
-                <span>FULL NAME: </span><input className=""/>
-                <hr/>
-                <span>ADDRESS: </span><input className=""/>
-                <hr/>
-                <span>ADDRESS 2: </span><input className=""/>
-                <hr/>
-                <span>CITY: </span><input className=""/>
-                <hr/>
-                <span>STATE: </span><input className=""/>
-                <hr/>
-                <span>ZIP CODE: </span><input className=""/>
-                <hr/>
-                <span >COUNTRY:</span><input className=""/>
-              </div>
+              <div>Add Address</div>
+              <div className="text-primary global_hover">Done</div>
             </div>
-          )
-        }
-       
+            <hr />
+            <div className="mx-5  address_form px-4">
+              <span>EMAIL: </span>
+              <input className="" />
+              <hr />
+              <span>FULL NAME: </span>
+              <input className="" />
+              <hr />
+              <span>ADDRESS: </span>
+              <input className="" />
+              <hr />
+              <span>ADDRESS 2: </span>
+              <input className="" />
+              <hr />
+              <span>CITY: </span>
+              <input className="" />
+              <hr />
+              <span>STATE: </span>
+              <input className="" />
+              <hr />
+              <span>ZIP CODE: </span>
+              <input className="" />
+              <hr />
+              <span>COUNTRY:</span>
+              <input className="" />
+            </div>
+          </div>
+        )}
       </div>
 
       {/* =====================Add payment method popup================================= */}
-      {
-        addPaymentMethodPopup && (
+      {addPaymentMethodPopup && (
+        <div>
+          <div className="add_payment_popup_parent">
+            <p className="global_hover">
+              <CancelIcon
+                style={{
+                  position: "absolute",
+                  marginTop: "-18px",
+                  marginLeft: "-18px",
+                }}
+                onClick={() => setaddPaymentMethodPopup(false)}
+              />{" "}
+            </p>
+            <div
+              className="m-4 global_hover"
+              onClick={() => setcreditCardMethod(true)}
+            >
+              <p className="fw-bold">
+                <AddIcon className="mx-3" /> Add Credit Card
+              </p>
+            </div>
+            <hr />
+            <div className="m-4 global_hover">
+              <p className="fw-bold">
+                <PaymentsIcon className="mx-3" />
+                Use Paypal
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {creditCardMethod && (
+        <div className="credit_card_popup_parent">
+          <div className="d-flex justify-content-between container px-3 mt-3 fw-bolder">
+            <div></div>
+            <div>Card Details</div>
+            <div
+              className="text-primary global_hover"
+              onClick={() =>
+                setcreditCardMethod(false) & setaddPaymentMethodPopup(false)
+              }
+            >
+              Done
+            </div>
+          </div>
+          <hr />
+
           <div>
-            <div className="add_payment_popup_parent">
-              <p className="global_hover"><CancelIcon  style={{ position:"absolute", marginTop:"-18px",marginLeft:"-18px" }}  onClick={()=>setaddPaymentMethodPopup(false)}/> </p>
-              <div className="m-4 global_hover" onClick={()=> setcreditCardMethod(true)} >
-                <p className="fw-bold" ><AddIcon className="mx-3" /> Add Credit Card</p>
-              </div>
-              <hr/>
-              <div className="m-4 global_hover">
-                <p className="fw-bold" ><PaymentsIcon className="mx-3" />Use Paypal</p>
-              </div>
-            </div>
+            <App />
           </div>
-        )
-      }
-
-      {
-        creditCardMethod && (
-          <div className="credit_card_popup_parent">
-            <div className="d-flex justify-content-between container px-3 mt-3 fw-bolder">
-              <div></div>
-              <div>Card Details</div>
-              <div className="text-primary global_hover"  onClick={()=> setcreditCardMethod(false) & setaddPaymentMethodPopup(false)}>Done</div>
-            </div>
-            <hr/>
-
-            <div>
-             <CreditCardInput
-            fieldClassName="input"
-            /></div>
-           
-          </div>
-        )
-      }
-
+        </div>
+      )}
     </div>
   );
 };
